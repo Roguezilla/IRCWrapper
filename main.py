b784311ac57ec46c592f3bdbda142f2de0d8c9f8
Olek <23708759+Roguezilla@users.noreply.github.com>
2018-12-29 20:37:19 +0000

    Minor improvements.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import threading
 class IRCWrapper():
 	def __init__(self, irc_socket):
 		self.irc_socket = irc_socket
+		self.irc_channel = ' '
 		print('Welcome to IRCWrapper by rogue')
 		print('You can change channels by using the SWC command.')
 
@@ -21,36 +22,39 @@ class IRCWrapper():
 
 	def pong(self):
 		didnt_pong = True
+		print('Trying to ping back.')
 		while didnt_pong:
 			data = self.irc_socket.recv(4096)
 			if data.find(b'PING :') != -1:
-				self.send_data('PONG ' + data.split()[9].decode('utf8') + '\r\n')
+				self.send_data('PONG ' + data.split()[len(data.split())-1].decode('utf8') + '\r\n')
 				didnt_pong = False
 		print('Pong!')
 
 	def join_channel(self, channel):
+		self.irc_channel = channel
 		self.send_data('JOIN {}'.format(channel))
 		print('Switched to channel: {}'.format(channel))
 
-	def send_user_msg(self):
+	def handle_user_commands(self):
 		while True:
 			buf = input('Command: ')
 			if buf == 'SWC':
 				irc.join_channel(input('Channel: '))
 			else:
-				self.send_data(buf)
+				self.send_data(buf.replace('say', 'PRIVMSG'))
 
 	def read_msgs(self):
 		while True:
 			msg = str.split(self.irc_socket.recv(1024).decode('utf8'))
-			if msg[1] == 'PRIVMSG':
-				print('\n {0} in {1}'.format(msg[0], ' '.join(msg[2:])))
+			if len(msg) > 3:
+				if msg[1] == 'PRIVMSG':
+					print('\n{0} in {1}'.format(msg[0].split('!')[0][1:], ' '.join(msg[2:]).replace(self.irc_channel + ' :', self.irc_channel + ' -> ')))
 
 irc = IRCWrapper(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
 irc.irc_conn('cathook.irc.inkcat.net', 8080)
 irc.login('roguebot')
 irc.pong()
-irc.join_channel('#cat_coms')
+irc.join_channel('#E')
 
-threading.Thread(target=irc.send_user_msg).start()
+threading.Thread(target=irc.handle_user_commands).start()
 threading.Thread(target=irc.read_msgs).start()
